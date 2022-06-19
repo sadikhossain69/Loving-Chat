7ec9642d7073fa65572b144faa56a0d2282052f5
sadikhossain69 <sadikhossain433@gmail.com>
2022-06-20 04:17:05 +0600

    password encrypted issue fixed

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -40,23 +40,23 @@ const registerUser = asyncHandler(async (req, res) => {
 })
 
 const authUser = asyncHandler(async (req, res) => {
-    const { email, password } = req.body
+    const { email, password } = req.body;
 
-    const user = User.findOne({ email }) 
+    const user = await User.findOne({ email });
 
     if (user && (await user.matchPassword(password))) {
         res.json({
             _id: user._id,
             name: user.name,
-            password: user.password,
+            email: user.email,
+            isAdmin: user.isAdmin,
             pic: user.pic,
-            token: generateToken(user._id)
-        })
+            token: generateToken(user._id),
+        });
     } else {
-        res.status(401)
-        throw new Error("Invalid Email or Password")
+        res.status(401);
+        throw new Error("Invalid Email or Password");
     }
-
-})
+});
 
 module.exports = { registerUser, authUser }
