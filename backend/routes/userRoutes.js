1dffbf5e13fe53b736da0b5f54469d8f5f8f5d9e
sadikhossain69 <sadikhossain433@gmail.com>
2022-06-22 02:22:14 +0600

    authorization middleware for searching users

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { registerUser, authUser, allUsers } = require('../controllers/userControllers');
+const project = require('../middleware/authMiddleware');
 const router = express.Router()
 
-router.route('/').post(registerUser).get(allUsers)
+router.route('/').post(registerUser).get(project, allUsers)
 router.post('/login', authUser)
 
 module.exports = router
