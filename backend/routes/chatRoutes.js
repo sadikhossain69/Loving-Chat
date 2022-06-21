c2c481b5e276a665cd1be42ca08cf2a309d01c25
sadikhossain69 <sadikhossain433@gmail.com>
2022-06-22 03:21:41 +0600

    creating chat api

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const { accessChat } = require('../controllers/chatControllers')
 const project = require('../middleware/authMiddleware')
 const router = express.Router()
 
-// router.route('/').post(project, accessChat)
+router.route('/').post(project, accessChat)
 // router.route('/').get(project, fetchChats)
 // router.route('/group').post(project, createGroupChat)
 // router.route('/groupremove').put(project, removeFromGroup)
